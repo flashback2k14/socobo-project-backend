92f8c0d89cb093f2ea45b8c0d48de5cab1a30552
Sebastian Kloppe <feedbackcalcest@gmail.com>
2016-03-08 20:51:13 +0100

    begin implement mailer function

--- api/v1/index.js
+++ api/v1/index.js
@@ -11,13 +11,36 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.get("/", function(req, res) {
-  console.log("Socobo Project Backend is ready to Rock!");
-  res.send("Socobo Project Backend is ready to Rock!");
-});
 app.get("/api/v1/", function(req, res) {
   console.log("Socobo Project Backend is ready to Rock!");
   res.send("Socobo Project Backend is ready to Rock!");
+});
+
+app.post("/api/v1/send-grocery-list", function(req, res) {
+  if (req.body.email.length === 0) {
+    res.status(500).send({"msg" : "No Email Address available to Mail!"});
+    return false;
+  }
+  
+  if (req.body.list.length === 0) {
+    res.status(500).send({"msg" : "No Grocery List Items available to Mail!"});
+    return false;
+  }
+  
+  // var emailText = "Grocery List Items:\n\n";
+  // req.body.list.forEach(function(item) {
+  //   emailText += "- " + item + "\n"
+  // });
+
+  // nodeMailer.mail({
+  //   from: "socobo.project@gmail.com",
+  //   to: req.body.email,
+  //   subject: "Socobo Project - Grocery List",
+  //   text: emailText
+  // });
+  
+  res.status(200).send({"msg": process.env.TEST});
+  //res.status(200).send({"msg": "Grocery List Items send to User"});
 });
 
 var server = http.createServer(app).listen(port, function() {
