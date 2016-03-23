779b378234cc44533531904cda847f644c21057d
Sebastian Kloppe <feedbackcalcest@gmail.com>
2016-03-23 17:09:11 +0100

    move request check to middleware function for send mail and add configs for debug, editor and npm tasks

--- app/logic/util.js
+++ app/logic/util.js
@@ -1,8 +1,46 @@
 module.exports = {
   /**
+   * Middleware to check request for the send mail request
+   * 
+   * @param {Object} req - Request
+   * @param {Object} res - Response
+   * @param {Function} next - Callback
+   * @returns {Boolean | Function}
+   * @function
+   */
+  checkRequest: function(req, res, next) {
+    // check if the request has data inside the body
+    if (req.get("X-SOCOBO-AUTH") === undefined) {
+        res.status(401).send({type: "error", msg: "Not authorized"})
+        return false;
+    }
+    if (req.body.length === 0) {
+      res.status(400).send({type: "error", msg: "No Request Data available to Mail!"});
+      return false;
+    }
+    // check if grocery list is available
+    if (req.body.list.length === 0) {
+      res.status(400).send({type: "error", msg: "No Grocery List Items available to Mail!"});
+      return false;
+    }
+    // go to next function
+    next();
+  },
+  
+  /**
+   * Return Auth Header Tag
+   * 
+   * @returns {String}
+   * @function
+   */
+  getAuthHeaderTag: function() {
+    return "X-SOCOBO-AUTH";
+  },
+  
+  /**
    * Generate Plain Email Body Text
    *
-   * @param itemList
+   * @param {Array} itemList
    * @returns {String}
    * @function
    */
@@ -17,13 +55,12 @@ module.exports = {
   /**
    * Generate HTML Email Body Text
    *
-   * @param itemList
+   * @param {Array} itemList
    * @returns {String}
    * @function
    */
   generateHtmlBodyText: function(itemList) {
     var emailText = "";
-
     emailText += "<h1>Socobo Project</h1>";
     emailText += "</br>"
     emailText += "</br>"
@@ -31,13 +68,13 @@ module.exports = {
     emailText += "</br>"
     emailText += "</br>"
     emailText += "<ul>"
-
+    
     itemList.forEach(function(item) {
       emailText += "<li>&#x95; " + item.desc + "</li>"
     });
-
+   
     emailText += "</ul>"
-
+    
     return emailText;
   }
 };
--- app/routes/api/v1/api-v1.js
+++ app/routes/api/v1/api-v1.js
@@ -6,30 +6,15 @@ var devEnv = require("../../../../dev-env.json");
 
 module.exports = function(express) {
   var api = express.Router();
-  const AUTH_HEADER = "X-SOCOBO-AUTH";
-
+  
   api.get("/", function(req, res) {
     console.log("Socobo Project Backend API is ready to Rock!");
     res.send("Socobo Project Backend API is ready to Rock!");
   });
-
-  api.post("/send-grocery-list", function(req, res) {
-    // check if the request has data inside the body
-    if (req.get(AUTH_HEADER) === undefined) {
-        res.status(401).send({type: "error", msg: "Not authorized"})
-        return false;
-    }
-    if (req.body.length === 0) {
-      res.status(400).send({type: "error", msg: "No Request Data available to Mail!"});
-      return false;
-    }
-    // check if grocery list is available
-    if (req.body.list.length === 0) {
-      res.status(400).send({type: "error", msg: "No Grocery List Items available to Mail!"});
-      return false;
-    }
-
-    user.getUserMail(req.get(AUTH_HEADER))
+  
+  api.post("/send-grocery-list", util.checkRequest, function(req, res) {
+    // get user email from firebase
+    user.getUserMail(req.get(util.getAuthHeaderTag()))
       .then((email) => {
         // get mailer config data
         var host = process.env.PROVIDER_HOST || devEnv.PROVIDER_HOST;
@@ -37,16 +22,12 @@ module.exports = function(express) {
         var useSsl = process.env.PROVIDER_USE_SSL || devEnv.PROVIDER_USE_SSL;
         var authUser = process.env.EMAIL_ADDRESS || devEnv.EMAIL_ADDRESS;
         var authPw = process.env.EMAIL_PASSWORD || devEnv.EMAIL_PASSWORD;
-
         // setup mailer
         mailer.setup(host, port, useSsl, authUser, authPw);
-
         // build html body
         var emailHtml = util.generateHtmlBodyText(req.body.list);
-
         // build email
         var mail = mailer.buildEmail(email, "Socobo Project - Grocery List", emailHtml);
-
         // send email
         mailer.send(mailer.getSmtpConfig(), mail, function(error, info) {
           if (error) {
@@ -57,6 +38,6 @@ module.exports = function(express) {
         
     }).catch((error) => res.status(500).json({type: error.code, msg: error.message}));
   });
-
+  
   return api;
 }
